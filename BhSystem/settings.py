fc77f6c428ecbe2c22d534605914ad49fbdc786c
mgamerxph <mgamerxph@gmail.com>
2025-07-13 23:05:45 +0800

    Update settings.py to use environment variables

--- BhSystem/settings.py
+++ BhSystem/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-930w2p5zldeli=wyv_l99y9yk5jul+#+w(c$jf=m-wtvk*bzjw'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
